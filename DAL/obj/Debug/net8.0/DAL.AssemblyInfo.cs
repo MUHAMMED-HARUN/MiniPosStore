7060e95e19a56f0e2a0e8d41fd81b76e6fd50e89
io <mm>
2025-09-06 15:48:29 +0300

    fghjr

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70a689589dced58b3d7fedc42d57e6417b04a5bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+201debb4454fe14167771edfb4be7d3d262aa456")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
